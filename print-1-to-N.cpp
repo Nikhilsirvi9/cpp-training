a216550a9ad8e18e23130931ef4a88ba949c7bb9
Nikhilsirvi9 <nikhilsirvi9@gmail.com>
2025-02-24 14:20:21 +0530

    first cpp program

---
+++ print-1-to-N.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    cout<<"enter the number";
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        cout<<i+1<<" ";
+    }
+}
